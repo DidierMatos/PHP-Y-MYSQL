03d0da98d942a98165618bef5100709e97b4bb5c
Didier Matos <didiermat00s@gmail.com>
2016-12-17 02:30:55 -0500

    Variables
    Se crearon variables, comillas dobles, simples y como ver el tipo de una
    variable

---
+++ variables.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+//String
+$nombre = 'Didier';
+//Integer
+$numero = 7;
+//Double
+$numero_decimal = 7.7;
+//Boolean
+$verdadero = true;
+
+echo $nombre;
+echo $numero;
+
+echo "Hola $nombre";
+echo 'Hola, ' . $nombre;
+
+//conocer el tipo de una variable
+echo gettype($nombre);
+
+?>
