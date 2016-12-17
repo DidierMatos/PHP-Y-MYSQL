142e1cbd433c3097a0965fe2c62ec2205f770f28
Didier Matos <didiermat00s@gmail.com>
2016-12-17 02:37:22 -0500

    Constantes
    se vio como se generan las constantes desde php con algunos consejos de
    comillas y mayusculas

---
+++ constantes.php
@@ -0,0 +1,16 @@
+<?php 
+
+define('PI',3.1416); // las constantes son globales
+//define('PI',ASDASDASD); no se puede sobrescribir
+define('NOMBRE','Didier');
+
+echo NOMBRE;
+
+echo PI;
+
+/*
+$nombre = "Carlos";
+$nombre = "Didier";
+echo $nombre;
+*/
+?>
