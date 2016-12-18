e4e29965340fbf58f758daf4027f3b935187073d
Didier Matos <didiermat00s@gmail.com>
2016-12-18 03:46:43 -0500

    Ciclo_For
    se agrego incrementos, decrementos, texto y multiplos en este ciclo FOR

---
+++ ciclo_for.php
@@ -0,0 +1,27 @@
+<?php
+
+//imprime texto
+for($i = 1; $i <= 10; $i++){
+	echo 'Hola <br/>';
+}
+/*separacion unicamente*/	echo '<br/>';
+
+//va incrementando de 1 hasta 10 
+for($i = 1; $i <= 10; $i++){
+	echo $i . '<br/>';
+}
+/*separacion unicamente*/	echo '<br/>';
+
+//va decrementando de 10 hasta 1
+for($i = 10; $i >= 1; $i--){
+	echo $i . '<br/>';
+}
+/*separacion unicamente*/	echo '<br/>';
+
+// va de 5 en 5
+for($i = 0 ; $i <= 100; $i += 5){
+	echo $i . '<br/>';
+}
+/*separacion unicamente*/	echo '<br/>';
+
+?>
