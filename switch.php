48a66b3833848549a14ce57b3e05bd76722c49ab
Didier Matos <didiermat00s@gmail.com>
2016-12-18 03:01:30 -0500

    Switch
    Se vio rapidamente la diferencia de switch y de las condicionales

---
+++ switch.php
@@ -0,0 +1,28 @@
+<?php
+
+$mes= 'Diciembre';
+
+switch($mes){
+	case 'Diciembre':
+		echo 'Feliz Navidad';
+	break;
+
+	case 'Enero':
+		echo 'Feliz Año Nuevo';
+	break;	
+
+	default:
+		echo 'En este mes no se celebra nada';
+	break;
+}
+
+if ($mes == 'Diciembre') {
+	echo 'Feliz Navidad';
+}else if ($mes == 'Enero') {
+	echo 'Feliz Año Nuevo';
+}else {
+	echo 'En este mes no se celebra nada';
+
+}
+
+?>
