24e0b8335fdbcd62697058becd22ea9cec294f8a
Didier Matos <didiermat00s@gmail.com>
2017-01-10 17:52:09 -0500

    Nombre y Comentarios
    Se actualizo el nombre y se añadieron mas comentarios

---
+++ 1.-hola_mundo.php
@@ -0,0 +1,32 @@
+<?php //Con este codigo inicializamos PHP
+
+// De esta manera podemos escribir un comentario en el codigo de una sola linea
+
+/*
+Este es un comentario
+de multiples lineas
+*/
+
+#Este es otro tipo de comentario de una sola linea
+
+// echo nos sirve para imprimir info en pantalla
+
+#echo "hola mundo";
+
+?> <!--Con este codigo cerramos el inicio de PHP-->
+
+
+<!--Este es un comentario 
+de multiples lineas en Html-->
+
+<!--IMPORTANTE: Aqui iniciamos la estructura HTML y dentro podemos utilizar el lenguaje PHP-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+		<h1><?php echo "Hola Mundo"; ?></h1>								
+</body>
+</html>
