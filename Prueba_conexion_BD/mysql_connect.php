3a0ee9b9477dddfb89013448a29f6631cea69904
Didier Matos <didiermat00s@gmail.com>
2017-02-27 15:52:38 -0500

    Mysql_Connect.php
    Se empezo a agregar comentarios en cada archivo para explicar con mas
    detalle lo que realiza el codigo

--- Prueba_conexion_BD/mysql_connect.php
+++ Prueba_conexion_BD/mysql_connect.php
@@ -1,15 +1,43 @@
 <?php
+#Modo Connection pero es antiguo ya no se recomienda por que se le puede inyectar codigo y alterar los datos
 
 $conexion = mysql_connect('localhost', 'root', '') or die ('No se pudo conectar a la bd'); //guarda la conexion con la direccion de host usuario y contraseña en una variable. En caso de fallar no permite accesar.
 
-mysql_select_db('prueba_datos.sql', $conexion); //agrega el nombre de la base de datos junto a la variable de conexion
+mysql_select_db('prueba_datos', $conexion); //agrega el nombre de la base de datos junto a la variable de conexion
 
-mysql_query('select * from usuarios'); //permite hacer consultas
+$resultados = mysql_query('select * from usuarios'); //permite hacer consultas
+
+//---------------------------------------------------------------------
+
+$fila = mysql_fetch_object($resultados); //traduce los resultados en un objeto y lo guarda en una variable
+
+echo $fila->nombre; // imprime el objeto recuperando una fila de resultados y este objeto incluye propiedades de la fila
+
+#Si se repiten estas 2 ultimas lineas de comandos imprimira las propiedades de las filas siguientes
 
 
-$fila = mysql_fetch_object($resultados); //traduce los resultados y lo guarda en una variable
+//---------------------------------------------------------------------
+echo '<br/>'.'<b>'.'Se puede hacer en un Ciclo While'.'<br/>'.'</b>';
+#Para ahorrar codigo y evitar que imprima false cuando no hayan valores se realizara con un ciclo WHILE
 
-echo fila->nombre;
+#Mediante OBJETOS
+echo '<br/>'.'<b>'.'Mediante Objetos'.'<br/>'.'</b>';
+while($fila = mysql_fetch_object($resultados)) { 
+	echo $fila->nombre;
+	echo '<br/>';
+
+}
+/*
+#Mediante ARREGLOS
+echo '<br/>'.'<b>'.'Mediante Arreglos'.'<br/>'.'</b>';
+
+while($fila = mysql_fetch_array($resultados)) { 
+	echo $fila['nombre'];
+	echo '<br/>';
+
+}
+*/
+
 
 ?>
 
