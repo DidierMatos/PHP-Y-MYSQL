728f27c8bfec77f68a9c30448e77b5ff2174de8d
Didier Matos <didiermat00s@gmail.com>
2017-04-10 02:02:43 -0500

    27.- Funciones_Matematicas
    Son funciones para redondear de forma oficial y teniendo un solo decimal
    despues del numero, para elegir al azar y explicacion de una manera
    diferente de imprimir constantes.

---
+++ Sintaxis/27.- Funciones_Matematicas.php
@@ -0,0 +1,31 @@
+<?php
+
+# FUNCION ROUND REDONDEA NUMEROS
+
+// $numero = 15.525;
+
+// echo round($numero, 1); // el segundo apartado nos permite mostrar el numero de decimales del numero.
+
+# FUNCION RAND PERMITE ELEGIR UN NUMERO AL AZAR 
+
+// $numero = rand(1,10); //parametros inicio y final 
+
+// echo $numero;
+
+
+#FUNCION CEIL REDONDEA UN NUMERO HACIA ARRIBA SIN IMPORTAR CUAL SEA EL DECIMAL ES DECIR 12.001 = 13 EJEMPLO PARA NUMERO DE PAGINAS EN UNA PAGINACION
+
+
+// $numero = 15.525;
+
+// echo ceil(12.1);
+
+
+# CONSTANTE
+
+// define('PI', 3.14) antiguamente
+
+echo M_PI;
+
+
+?>
